6849d4a647ca9bad966315b25de565babd8e3f88
Dimelza Paola Salazar Apaza <paodi@MBP-de-Dimelza.local>
2025-05-20 03:29:34 -0400

    sitio reyna vip

--- cabecera.php
+++ cabecera.php
@@ -1,3 +1,4 @@
+ <link rel="shortcut icon" href="images/favicon.ico">
  <link href="./css/styles.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
