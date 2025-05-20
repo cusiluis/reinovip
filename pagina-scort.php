8ab81af55c009343c704f96a892cc881408fde63
Dimelza Paola Salazar Apaza <paodi@MBP-de-Dimelza.local>
2025-05-20 00:40:11 -0400

    sitio reyna vip

--- pagina-scort.php
+++ pagina-scort.php
@@ -66,6 +66,18 @@
           <a href="http://reinovip.com/fotos/1200_480496.jpg" class="glightbox" data-gallery="perfil">
             <img src="http://reinovip.com/fotos/1200_480496.jpg" alt="Mini 5">
           </a>
+          <a href="http://reinovip.com/fotos/1200_480496.jpg" class="glightbox" data-gallery="perfil">
+            <img src="http://reinovip.com/fotos/1200_480496.jpg" alt="Mini 5">
+          </a>
+          <a href="http://reinovip.com/fotos/1200_480496.jpg" class="glightbox" data-gallery="perfil">
+            <img src="http://reinovip.com/fotos/1200_480496.jpg" alt="Mini 5">
+          </a>
+          <a href="http://reinovip.com/fotos/1200_480496.jpg" class="glightbox" data-gallery="perfil">
+            <img src="http://reinovip.com/fotos/1200_480496.jpg" alt="Mini 5">
+          </a>
+          <a href="http://reinovip.com/fotos/1200_480496.jpg" class="glightbox" data-gallery="perfil">
+            <img src="http://reinovip.com/fotos/1200_480496.jpg" alt="Mini 5">
+          </a>
         </div>
       </div>
 
@@ -76,73 +88,78 @@
 
       <!-- PERFIL -->
       <div class="section-box">
-        <h3>Linda</h3>
+        <h1>Ana paula</h1>
         <p>
-          Una escort brasileña, ofrezco encuentro íntimo en mi piso limpio y discreto...
-          hola .. recién llegada por aquí dispuesta a complacerte en todo cariñosa, besucona apasionada dulce mimosilla viciosa morbosa ofrezco masajes eroticos, francés natural hasta el final, caricias duchita y hacemos el amor bien rico. ven a visitarme estoy en un piso privado muy limpio y discreto   
-        </p>
+         Amor soy ana pau ? puro deseo en la cama, caliente, viciosa, sensual y folladora. labios carnosos experta en mamadas hasta el final. ?coñito caliente me gusta hacer de todo, fogosa muy implicada mamadas profundas. ? pechos enormes y naturales ? completisima, me encanta hacer de todo pero sin prisas, quiero que pruebes mi francés. </p>
       </div>
 
       <!-- DATOS PERSONALES -->
       <div class="section-box">
-        <h4 class="section-title">Datos personales</h4>
+        <h2 class="section-title">Datos personales</h2>
           <div class="columns">
       <div class="column">
         <ul>
-          <li><span class="txt-datos">Edad:</span><span class="txt-valor">21</span></li>
-          <li><span class="txt-datos">Altura:</span><span class="txt-valor">173 cm</span></li>
-          <li><span class="txt-datos">Peso:</span><span class="txt-valor">53 kg</span></li>
-          <li><span class="txt-datos">Cabello:</span><span class="txt-valor">Castaño</span></li>
-          <li><span class="txt-datos">Ojos:</span><span class="txt-valor">Marrones</span></li>
-        </ul>
+          <li><span class="txt-datos">Edad:</span><span class="txt-valor">28</span></li>
+          <li><span class="txt-datos">Medidas:</span><span class="txt-valor">90-60-85</span></li>
+          <li><span class="txt-datos">Altura:</span><span class="txt-valor">1.65 cm</span></li>
+          <li><span class="txt-datos">Peso:</span><span class="txt-valor">58 kg</span></li>
+          
+                  </ul>
       </div>
       <div class="column">
         <ul>
-          <li><span class="txt-datos">Nacionalidad:</span><span class="txt-valor">Rusa</span></li>
-          <li><span class="txt-datos">Idiomas:</span><span class="txt-valor">Inglés, Ruso</span></li>
-          <li><span class="txt-datos">Fuma:</span><span class="txt-valor">No</span></li>
-          <li><span class="txt-datos">Tatuajes:</span><span class="txt-valor">No</span></li>
-          <li><span class="txt-datos">Piercings:</span><span class="txt-valor">Sí</span></li>
-        </ul>
+          <li><span class="txt-datos">Cabello:</span><span class="txt-valor">Rubia</span></li>
+          <li><span class="txt-datos">Ojos:</span><span class="txt-valor">Oscuros</span></li>
+          <li><span class="txt-datos">Nacionalidad:</span><span class="txt-valor">Española</span></li>
+          <li><span class="txt-datos">Idiomas:</span><span class="txt-valor">Español</span></li>
+          </ul>
       </div>
     </div>
 </div>
-      <!-- DISPONIBILIDAD -->
-      <div class="section-box">
-        <h4 class="section-title"><i class="far fa-clock" style="color: #793a57;font-size: 17px;"></i> Horarios de trabajo y Tarifas</h4>
-          <p> Disponible 24 horas</p>
-         <h4 class="section-title">Formas de pago</h4>
-          <p><i class="fas fa-money-check-alt" style="color: #793a57;font-size: 17px;"></i>  Pago en efectivo</p>
-          <h4 class="section-title">Lugares de Atención:</h4>
-          <p><i class="fas fa-hotel" style="color: #793a57;font-size: 17px;"></i>  Salidas a hoteles y pisos privados</p>
-      
-      </div>
 
-      <!-- SERVICIOS -->
+ <!-- SERVICIOS -->
       <div class="section-box">
-        <h4 class="section-title">Servicios</h4>
+        <h2 class="section-title">Servicios</h2>
         <div class="row small">
           <div class="col-sm-6">
-            <p>Masajes relajantes</p>
-              <p>Masaje prostático</p>
-              <p>Masajes eróticos</p>
-              <p>Masaje anal</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Trí­os</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Sado Light</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Masaje relajantes</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Masaje </p>
             
           </div>
           <div class="col-sm-6">
-             <p>Besos</p>
-              <p>Juguetes</p>
-              <p>Frances natural</p>
-              <p>6g</p>
-                      </div>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Juegos Eróticos</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Francés Natural</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Frances </p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Fetichismo</p>
+          </div>
+           <div class="col-sm-6">
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Dúplex</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> Cubana</p>
+            <p><i class="fas fa-check" style="color: #793a57;font-size: 17px;"></i> 69</p>
+          </div>
         </div>
       </div>
 
+      <!-- DISPONIBILIDAD -->
+      <div class="section-box">
+        <h3 class="section-title"><i class="fas fa-clock" style="color: #793a57;font-size: 17px;"></i> Horarios de trabajo y Tarifas</h3>
+          <p> Disponible 24 horas</p>
+         <h3 class="section-title"><i class="fas fa-money-check-alt" style="color: #793a57;font-size: 17px;"></i> Formas de pago</h3>
+          <p>  Pago en efectivo</p>
+          <h3 class="section-title"><i class="fas fa-hotel" style="color: #793a57;font-size: 17px;"></i> Lugares de Atención:</h3>
+          <p> Salida a Hoteles - Piso Privado - Salidas a Domicilio </p>
+      
+      </div>
+
+     
       <!-- CONTACTO -->
       <div class="section-box d-flex justify-content-between align-items-center">
-        <button class="btn btn-chat px-4"><i class="fas fa-mobile-alt" style="color: #793a57;font-size: 17px;"></i>  611 318 251 - CHAT</button>
-        <span class="small"> <i class="fas fa-map-marker-alt" style="color: #793a57;font-size: 17px;"></i>  España - Madrid</span>
-      </div>
+         <a class="btn-chat"><svg viewBox="0 0 256 259" width="30" height="30" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path d="m67.663 221.823 4.185 2.093c17.44 10.463 36.971 15.346 56.503 15.346 61.385 0 111.609-50.224 111.609-111.609 0-29.297-11.859-57.897-32.785-78.824-20.927-20.927-48.83-32.785-78.824-32.785-61.385 0-111.61 50.224-110.912 112.307 0 20.926 6.278 41.156 16.741 58.594l2.79 4.186-11.16 41.156 41.853-10.464Z" fill="#00E676"/><path d="M219.033 37.668C195.316 13.254 162.531 0 129.048 0 57.898 0 .698 57.897 1.395 128.35c0 22.322 6.278 43.947 16.742 63.478L0 258.096l67.663-17.439c18.834 10.464 39.76 15.347 60.688 15.347 70.453 0 127.653-57.898 127.653-128.35 0-34.181-13.254-66.269-36.97-89.986ZM129.048 234.38c-18.834 0-37.668-4.882-53.712-14.648l-4.185-2.093-40.458 10.463 10.463-39.76-2.79-4.186C7.673 134.63 22.322 69.058 72.546 38.365c50.224-30.692 115.097-16.043 145.79 34.181 30.692 50.224 16.043 115.097-34.18 145.79-16.045 10.463-35.576 16.043-55.108 16.043Zm61.385-77.428-7.673-3.488s-11.16-4.883-18.136-8.371c-.698 0-1.395-.698-2.093-.698-2.093 0-3.488.698-4.883 1.396 0 0-.697.697-10.463 11.858-.698 1.395-2.093 2.093-3.488 2.093h-.698c-.697 0-2.092-.698-2.79-1.395l-3.488-1.395c-7.673-3.488-14.648-7.674-20.229-13.254-1.395-1.395-3.488-2.79-4.883-4.185-4.883-4.883-9.766-10.464-13.253-16.742l-.698-1.395c-.697-.698-.697-1.395-1.395-2.79 0-1.395 0-2.79.698-3.488 0 0 2.79-3.488 4.882-5.58 1.396-1.396 2.093-3.488 3.488-4.883 1.395-2.093 2.093-4.883 1.395-6.976-.697-3.488-9.068-22.322-11.16-26.507-1.396-2.093-2.79-2.79-4.883-3.488H83.01c-1.396 0-2.79.698-4.186.698l-.698.697c-1.395.698-2.79 2.093-4.185 2.79-1.395 1.396-2.093 2.79-3.488 4.186-4.883 6.278-7.673 13.951-7.673 21.624 0 5.58 1.395 11.161 3.488 16.044l.698 2.093c6.278 13.253 14.648 25.112 25.81 35.575l2.79 2.79c2.092 2.093 4.185 3.488 5.58 5.58 14.649 12.557 31.39 21.625 50.224 26.508 2.093.697 4.883.697 6.976 1.395h6.975c3.488 0 7.673-1.395 10.464-2.79 2.092-1.395 3.487-1.395 4.882-2.79l1.396-1.396c1.395-1.395 2.79-2.092 4.185-3.487 1.395-1.395 2.79-2.79 3.488-4.186 1.395-2.79 2.092-6.278 2.79-9.765v-4.883s-.698-.698-2.093-1.395Z" fill="#FFF"/></svg>  +671 244 327 </a>
+         <span > <i class="fas fa-map-marker-alt" style="color: #793a57;font-size: 20px;"></i>  España - 7 palmas</span>
+         
+        </div>
 
     </div>
   </div>
